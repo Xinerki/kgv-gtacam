060d7b8f608b3ac03abe9b69036051b93d0059bf
xinerki <simasadves@gmail.com>
2023-06-12 17:31:47 +0200

    no more raycast jitter

--- client.lua
+++ client.lua
@@ -509,10 +509,8 @@ function processCustomTPCam(cam)
 		-- dist_scale = math.min(dist_scale + GetFrameTime() * 2.0, 1.0)
 		dist_scale = lerp(dist_scale, 1.0, GetFrameTime() * 2.0)
 	else
-		dist_scale = dist
+		dist_scale = lerp(dist_scale, dist, GetFrameTime() * 50.0)
 	end
-	
-	-- TODO: still a little jittery
 	
 	-- if hit ~= 0 and hitEnt ~= PlayerPedId() then
 		SetCamCoord(cam, lerp(pos, camPos+vec(0.0, 0.0, 1.0-dist_scale), dist_scale * 0.99))
